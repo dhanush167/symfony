a72554fd28c49e712a5670ba864c62dc2016c32d
kadawatha <bydhanushka@gmail.com>
2019-11-07 12:24:22 +0530

    MainController.php modify with route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
         //http://symfony.test/index.php/main
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+//        return $this->render('main/index.html.twig', [
+//            'controller_name' => 'MainController',
+//        ]);
+        return new Response();
     }
 }
